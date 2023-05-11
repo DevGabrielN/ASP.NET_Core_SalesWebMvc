8d682688767efdadb73f2d8e74cbfa0220674d94
Gabriel Nunes <leirbagmelo2@gmail.com>
2023-05-11 20:46:48 -0300

    Tratamento de exceção para delete (integridade referencial)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -33,9 +33,15 @@ namespace SalesWebMvc.Services
 
         public async Task RemoveAsync(int id)
         {
+            try { 
             Seller seller = await _context.Seller.FindAsync(id);
             _context.Seller.Remove(seller);
             await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException(e.Message);
+            }
         }
 
         public async Task UpdateAsync(Seller seller)
